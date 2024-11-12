b1c6c6091d7dea1a69a7579f241dadf32870797e
Syah-Git <aszaszrul@gmail.com>
2024-11-12 15:33:20 +0800

    Early Project

--- App.tsx
+++ App.tsx
@@ -104,6 +104,7 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    fontFamily: 'UrbanistBold',
   },
   sectionDescription: {
     marginTop: 8,
