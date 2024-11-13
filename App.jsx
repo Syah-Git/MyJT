92d5c5c922fdb10298256fe15352fec979aabaad
Syah-Git <aszaszrul@gmail.com>
2024-11-13 13:37:40 +0800

    latest 13/11 1:37pm

--- App.jsx
+++ App.jsx
@@ -1,56 +1,57 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import NearbyScreen from './src/screens/NearbyScreen';
 import ScanScreen from './src/screens/ScanScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Entypo from 'react-native-vector-icons/Entypo';
-
-// Import your custom icon image
-import HomeFocusedIcon from './src/assets/icon/homeicon.svg';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
+
+// Helper function for icons
+const getTabIcon = (routeName, focused, color) => {
+  const size = styles.iconSize.size;
+
+  switch (routeName) {
+    case 'Home':
+     return focused ? (
+        <Octicons name="home" size={size} color={color} />
+      ) : (
+        <Octicons name="home" size={size} color={color} />
+      );
+    case 'Nearby':
+      return focused ? (
+        <Entypo name="location" size={size} color={color} />
+      ) : (
+        <Entypo name="location" size={size} color={color} />
+      );
+    case 'Scan':
+      return focused ? (
+        <MaterialCommunityIcons name="line-scan" size={23} color={color} />
+      ) : (
+        <MaterialCommunityIcons name="scan-helper" size={21} color={color} />
+      );
+    case 'Profile':
+      return focused ? (
+        <Octicons name="person-fill" size={size} color={color} />
+      ) : (
+        <Octicons name="person" size={size} color={color} />
+      );
+    default:
+      return null;
+  }
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            size = styles.iconSize.size;
-
-            // Define icons for each tab based on the focused state
-            if (route.name === 'Home') {
-              // Use custom image icon for focused Home tab
-              return focused ? (
-                <Image source={HomeFocusedIcon} style={styles.customIcon} />
-              ) : (
-                <Octicons name="home" size={size} color={color} />
-              );
-            } else if (route.name === 'Nearby') {
-              return focused ? (
-                <Entypo name="location-pin" size={size} color={color} />
-              ) : (
-                <Entypo name="location" size={size} color={color} />
-              );
-            } else if (route.name === 'Scan') {
-              return focused ? (
-                <Ionicons name="scan-circle" size={size} color={color} />
-              ) : (
-                <Ionicons name="scan" size={size} color={color} />
-              );
-            } else if (route.name === 'Profile') {
-              return focused ? (
-                <Octicons name="person-fill" size={size} color={color} />
-              ) : (
-                <Octicons name="person" size={size} color={color} />
-              );
-            }
-          },
+          tabBarIcon: ({ focused, color }) => getTabIcon(route.name, focused, color),
           tabBarActiveTintColor: '#17467C',
           tabBarInactiveTintColor: '#9E9E9E',
           headerShown: false,
@@ -71,7 +72,7 @@ export default function App() {
 // StyleSheet
 const styles = StyleSheet.create({
   tabBarStyle: {
-    height: 65,
+    height: 70,
     paddingBottom: 10,
     paddingTop: 10,
   },
@@ -87,8 +88,8 @@ const styles = StyleSheet.create({
     size: 22,
   },
   customIcon: {
-    width: 24, // Adjust width of the custom icon
-    height: 24, // Adjust height of the custom icon
+    width: 24,
+    height: 24,
     resizeMode: 'contain',
   },
 });
--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,5 @@
+
+// Old Code
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
