95d0850e0a8bf5f3f0fde45e9b3e768d50ba42b5
Syah-Git <aszaszrul@gmail.com>
2024-11-13 11:45:16 +0800

    app before button push fill

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import NearbyScreen from './src/screens/NearbyScreen';
 import ScanScreen from './src/screens/ScanScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Octicons from 'react-native-vector-icons/Octicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -16,33 +19,23 @@ export default function App() {
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ color, size }) => {
-            size = 22; // Adjust the icon size here
+            size = styles.iconSize.size; // Use icon size from styles
             if (route.name === 'Home') {
               return <Octicons name="home" size={size} color={color} />;
             } else if (route.name === 'Nearby') {
-              return <Ionicons name="location-outline" size={size} color={color} />;
+              return <Entypo name="location" size={size} color={color} />;
             } else if (route.name === 'Scan') {
-              return <Octicons name="scan" size={size} color={color} />;
+              return <Ionicons name="scan" size={size} color={color} />;
             } else if (route.name === 'Profile') {
-              return <Ionicons name="person-circle-outline" size={size} color={color} />;
+              return <Octicons name="person" size={size} color={color} />;
             }
           },
           tabBarActiveTintColor: '#17467C',
-          tabBarInactiveTintColor: '#6b6b6b',
+          tabBarInactiveTintColor: '#9E9E9E',
           headerShown: false,
-          tabBarStyle: {
-            height: 70, // Adjust the height of the tab bar
-            paddingBottom: 15, // Space between icon and bottom edge
-            paddingTop: 10, // Space between top edge and icon
-          },
-          tabBarIconStyle: {
-            marginBottom: -4, // Adjust the space between icon and label
-          },
-          tabBarLabelStyle: {
-            fontFamily: 'MyCustomFont', // Your custom font
-            fontSize: 14, // Adjust the font size
-            marginTop: -6, // Adjust the space between label and icon
-          },
+          tabBarStyle: styles.tabBarStyle,
+          tabBarIconStyle: styles.tabBarIconStyle,
+          tabBarLabelStyle: styles.tabBarLabelStyle,
         })}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
@@ -53,3 +46,23 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+//StyleSheet
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 65, // Adjust the height of the tab bar
+    paddingBottom: 10, // Space between icon and bottom edge
+    paddingTop: 10, // Space between top edge and icon
+  },
+  tabBarIconStyle: {
+    marginBottom: 0, // Adjust the space between icon and label
+  },
+  tabBarLabelStyle: {
+    fontFamily: 'UrbanistSemiBold', // Your custom font
+    fontSize: 12, // Adjust the font size
+    marginTop: -2, // Adjust the space between label and icon
+  },
+  iconSize: {
+    size: 22, // Define icon size here
+  },
+});
